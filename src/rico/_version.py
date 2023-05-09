80b016d523608310a07a1dc5921517dead875bf1
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2023-05-09 21:25:33 +0600

    Fix package version

--- src/rico/_version.py
+++ src/rico/_version.py
@@ -1,2 +1,15 @@
 """Package version."""
-__version__ = "0.0.3"
+
+import importlib.metadata
+import importlib.resources
+
+
+try:
+    __version__ = importlib.metadata.version(__package__ or "rico")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = (
+        importlib.resources.files("rico")
+        .joinpath("_version.txt")
+        .read_text()
+        .strip()
+    )
