9362046192709ad89dde1cd4675b525a5140062f
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2023-05-13 14:04:55 +0600

    Parser returns a list of elements without root

--- src/rico/html.py
+++ src/rico/html.py
@@ -3,62 +3,51 @@
 from __future__ import annotations
 
 import html.parser
-from typing import TYPE_CHECKING
 import xml.etree.ElementTree as ET  # noqa: N817
 
 
-if TYPE_CHECKING:
-    from typing import Any
-
-
 class HTMLParser(html.parser.HTMLParser):
-    """Simple HTML parser. Returns an instance of ET.Element on close().
+    """Simple HTML parser. Returns a list of instances of ET.Element on close().
 
     Ignores comments, doctype declaration and processing instructions.
     """
 
-    def __init__(self, root: str = "div"):
-        """Initalizes parser.
-
-        Args:
-            root: A root tag.
-        """
+    def __init__(self):  # noqa: D107
         super().__init__()
-        self._root = root
+        self._root = "root"
         self._builder = ET.TreeBuilder()
         self._builder.start(self._root, {})
 
-    def handle_starttag(  # noqa: ANN201, D102
+    def handle_starttag(  # noqa: D102
         self,
         tag: str,
         attrs: list[tuple[str, str | None]],
-    ):
+    ) -> None:
         self._builder.start(tag, dict(attrs))
 
-    def handle_endtag(self, tag: str):  # noqa: ANN201, D102
+    def handle_endtag(self, tag: str) -> None:  # noqa: D102
         self._builder.end(tag)
 
-    def handle_data(self, data: Any):  # noqa: ANN201, D102
+    def handle_data(self, data: str) -> None:  # noqa: D102
         self._builder.data(data)
 
-    def close(self) -> ET.Element:  # noqa: D102
+    def close(self) -> list[ET.Element]:  # noqa: D102
         super().close()
         self._builder.end(self._root)
-        return self._builder.close()
+        return list(self._builder.close())
 
 
-def parse_html(data: str, root: str = "div") -> ET.Element:
+def parse_html(data: str) -> list[ET.Element]:
     """Parse HTML from a string.
 
     Ignores comments, doctype declaration and processing instructions.
 
     Args:
         data: HTML data.
-        root: A root tag.
 
     Returns:
         Parsed HTML data.
     """
-    parser = HTMLParser(root=root)
+    parser = HTMLParser()
     parser.feed(data)
     return parser.close()
