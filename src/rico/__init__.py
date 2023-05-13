009d84718d171a8b1da23e4664e831f1cc02c3ee
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2023-05-13 20:54:30 +0600

    Import rico.html symbols in __init__.py

--- src/rico/__init__.py
+++ src/rico/__init__.py
@@ -1,4 +1,6 @@
 # ruff: noqa: F401
+# pyright: reportUnusedImport=false
 """Rich content to HTML as easy as doc.print(x)."""
 
-from ._version import __version__
+from rico._version import __version__
+from rico.html import HTMLParser, indent_html, parse_html, serialize_html
