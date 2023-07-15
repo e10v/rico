2ad5be3da53318674e0c9eaacfbe5eba68f90b29
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2023-07-15 18:31:37 +0600

    Create examples

---
+++ examples/10_basic_usage.py
@@ -0,0 +1,27 @@
+"""Basic usage examples."""
+
+import pandas as pd
+
+import rico
+
+
+if __name__ == "__main__":
+    df = pd.DataFrame({
+        "a": list("CCCDDDEEE"),
+        "b": [2, 7, 4, 1, 2, 6, 8, 4, 7],
+    })
+    plot = df.plot.scatter(x="a", y="b")  # type: ignore
+
+    # Declarative API
+    doc = rico.Doc("Hello world!", df, plot, title="My doc")
+
+    with open(__file__[0:-3] + "_declarative.html", "w") as f:
+        f.write(doc.serialize(indent=True))
+
+    # Imperative API
+    doc = rico.Doc(title="My doc")
+    doc.append("Hello world!")
+    doc.append(df, plot)
+
+    with open(__file__[0:-3] + "_imperative.html", "w") as f:
+        f.write(doc.serialize(indent=True))
---
+++ examples/20_serialization.py
@@ -0,0 +1,31 @@
+"""Serialization examples."""
+
+import pandas as pd
+
+import rico
+
+
+if __name__ == "__main__":
+    df = pd.DataFrame({
+        "a": list("CCCDDDEEE"),
+        "b": [2, 7, 4, 1, 2, 6, 8, 4, 7],
+    })
+    plot = df.plot.scatter(x="a", y="b")  # type: ignore
+
+    doc = rico.Doc("Hello world!", df, plot, title="My doc")
+
+    # Default
+    with open(__file__[0:-3] + "_default.html", "w") as f:
+        f.write(str(doc))
+
+    # Indent
+    with open(__file__[0:-3] + "_indent.html", "w") as f:
+        f.write(doc.serialize(indent=True))
+
+    # Indent with custom space
+    with open(__file__[0:-3] + "_indent_space.html", "w") as f:
+        f.write(doc.serialize(indent=True, space="    "))
+
+    # Remove unnecessary whitespace
+    with open(__file__[0:-3] + "_strip.html", "w") as f:
+        f.write(doc.serialize(strip=True))
