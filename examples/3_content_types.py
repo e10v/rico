14dbcc92614ed3fd58d448d3dea644cf00eceaab
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2023-07-23 13:42:40 +0600

    Update examples

--- examples/3_content_types.py
+++ examples/3_content_types.py
@@ -12,16 +12,27 @@ if __name__ == "__main__":
     })
     plot = df.plot.scatter(x="a", y="b")  # type: ignore
 
+    # Change the default behavior
     doc = rico.Doc(
         rico.Text("Hello world!", mono=True),  # The default value is False.
         df,
         rico.Plot(plot, format="png"),  # The default value is "svg".
-        title="Content types",
+        title="Content classes parameters",
     )
 
-    with open(__file__[:-3] + "1.html", "w") as f:
+    with open(__file__[:-3] + "_params_classes.html", "w") as f:
         f.write(doc.serialize(indent=True))
 
+    # Change the default behavior
+    doc = rico.Doc(title="Content methods parameters")
+    doc.append_text("Hello world!", mono=True)
+    doc.append(df)
+    doc.append_plot(plot, format="png")
+
+    with open(__file__[:-3] + "_params_methods.html", "w") as f:
+        f.write(doc.serialize(indent=True))
+
+    # Content specific classes
     doc = rico.Doc(
         rico.Markdown("## Dataframe"),
         df,
@@ -32,5 +43,16 @@ if __name__ == "__main__":
         title="Content types",
     )
 
-    with open(__file__[:-3] + "2.html", "w") as f:
+    with open(__file__[:-3] + "_specific_classes.html", "w") as f:
+        f.write(doc.serialize(indent=True))
+
+    doc = rico.Doc(title="Content types")
+    doc.append_markdown("## Dataframe")
+    doc.append(df)
+    doc.append_tag("h2", "Plot")
+    doc.append(plot)
+    doc.append_html("<h2>Code</h2>")
+    doc.append_code("print('Hello world!')")
+
+    with open(__file__[:-3] + "_specific_methods.html", "w") as f:
         f.write(doc.serialize(indent=True))
